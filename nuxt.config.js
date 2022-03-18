094fc38655047f17edd9aed2c800089d97609220
Jon Hadden <jhadden@vectice.com>
2022-03-17 23:56:59 -0500

    removing favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,9 +17,6 @@ export default {
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
   },
 
   router: {
